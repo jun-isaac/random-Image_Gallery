f7cc60420055a46e96ff452aee9723ff56fb9df0
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-06-21 12:30:45 +0900

    완료
    - 사진 랜덤 변경
    - 사진 레이어 기능으로 필터 추가
    - 날짜 변경

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Random Image Gallery
-https://jun-isaac.github.io/random-Image_Gallery/
 
 ## 소개
 - 랜덤하게 이미지가 변경되는 갤러리
@@ -8,6 +7,7 @@ https://jun-isaac.github.io/random-Image_Gallery/
 ## 사용기술
 - CSS Flex Layout
 - 자바스크립트 Math객체, Date객체
+- 타이머 함수(setInterval) - 자동재생
 
 ## 플러그인/라이브러리
 - 제이쿼리
---
+++ js/app.js
@@ -0,0 +1,46 @@
+// 이미지 변경 함수
+const img_url = [
+    'image/img01.jpg',
+    'image/img02.jpg',
+    'image/img03.jpg',
+    'image/img04.jpg',
+    'image/img05.jpg',
+    'image/img06.jpg',
+];
+
+
+$(function () {
+
+    // 년/월/일
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth() + 1;
+    let date = d.getDate();
+    console.log(year, month, date);
+
+    // html에 날짜 표시
+    $('.year').html(year);
+    $('.month').html(month);
+    $('.date').html(date);
+    
+
+        function changeBg() {
+
+            // 0 ~ 2 랜덤값 생성 (이미지 번호)
+            let imgNum = Math.floor(Math.random() * 6)
+            console.log(imgNum);
+
+            // 이미지 변경
+            $('body').css({
+                background: `url(image/filter1.png),
+                url(${img_url[imgNum]})`
+            })
+}
+
+
+// setInterval(함수, 시간)
+setInterval(changeBg, 5000);
+
+});
+
+// 응용과제: 이미지를 총 6장으로 추가 랜덤 수정
